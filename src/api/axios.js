fe41a9203a4429cdb47ffb7c18684d35b89d5281
WinningCloud <farhan84897@gmail.com>
2026-01-28 00:18:46 +0530

    Implemented backend url env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  
 });
+console.log("ENV URL:", import.meta.env.VITE_API_BASE_URL);
+
 
 // 🔥 THIS RUNS ON EVERY REQUEST
 api.interceptors.request.use((config) => {
